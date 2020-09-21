d609fa29240355d7e04b62366e201acf15513f66
Carlos <carlosmorenogarcia8@gmail.com>
2020-09-21 18:16:35 +0200

    menu home optimizado con un js del titulo que aparezca y desaparezca

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const titulo = document.querySelector('.titulo')
+
+const showDreamCodeCreate = () => {
+    titulo.style.opacity='1';
+    //titulo.style.width="50%";
+    
+}
+
+const hideDreamCodeCreate = () => {
+    //titulo.style.width="0%";
+    titulo.style.opacity='0';
+}
+
